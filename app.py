f50559071fd19118dabd73b1aeddccd307a77807
sun <thesunburns@outlook.com>
2025-01-14 13:21:56 +0100

    created lading page

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Hello',
+    page_icon='👋',
+)
+
+st.write('# Thank you for visiting! 🤖')
+
+st.sidebar.success('Select a demo above.')
+
+st.markdown(
+    '''
+    Here you'll find exciting project demos that truly inspire me, 
+    as well as a collection of posts I maintain to help guide my ongoing 
+    learning journey.
+    
+    I hope these resources provide valuable insights into my development 
+    process.
+
+    If you have any questions or feedback, feel free to reach out! I'm always happy to connect and chat
+    
+    I'm always open to connecting!
+
+    KD,  
+    L
+'''
+)
