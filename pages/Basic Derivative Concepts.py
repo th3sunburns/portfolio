74e556fd49587e1987bbd7978901e953c311a9a3
sun <thesunburns@outlook.com>
2025-01-14 14:25:39 +0100

    landing page now is a highlight

---
+++ pages/Basic Derivative Concepts.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Basic Derivative Concepts',
+    page_icon='',
+)
+
+st.write('# Basic Derivative Concepts')
+
+st.markdown(
+    '''
+
+'''
+)
