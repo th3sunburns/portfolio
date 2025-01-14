17892dd284dd8a45e6f6137a7b3df58e7ff29ae9
sun <thesunburns@outlook.com>
2025-01-14 14:17:43 +0100

    landing page now is a highlight

--- app.py
+++ app.py
@@ -1,36 +1,16 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title='Hello',
-    page_icon='👋',
+    page_title='Highlights',
+    page_icon='✨',
 )
 
-st.write('# Thank you for visiting! 🤖')
+st.write('# Highlights')
 
 st.sidebar.success('Select a demo above.')
 
 st.markdown(
     '''
-    Here you'll find exciting project demos that truly inspire me, 
-    as well as a collection of posts I maintain to help guide my ongoing 
-    learning journey.
     
-    I hope these resources provide valuable insights into my development 
-    process.
-
-    This is a Python-based application designed to demonstrate how communication 
-    between Finance, IT, and Backoffice teams can be made clear and comprehensible for everyone 
-    involved. While this is just a small demonstration, it highlights an essential approach to 
-    ensuring smooth communication flows within teams.
-
-    Effective communication enables quick reactions when necessary, ensuring that all departments 
-    are aligned and responsive in a fast-paced environment.
-
-    If you have any questions or feedback, feel free to reach out! I'm always happy to connect and chat
-    
-    I'm always open to connecting!
-
-    KD,  
-    L
-'''
+    '''
 )
